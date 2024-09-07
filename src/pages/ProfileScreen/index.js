9f4c811006cd11c9344560fb03d848f7a5e13c09
Robson-Higa <G38T47ss>
2024-09-06 20:07:34 -0400

    ffff

--- src/pages/ProfileScreen/index.js
+++ src/pages/ProfileScreen/index.js
@@ -5,9 +5,9 @@ import { styles } from './style';
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text styl={styles.title}>Editor de Texto</Text>
+      <Text styl={styles.title}>Editor ddddde Texto</Text>
 
-      
+
 
     </View>
   );
